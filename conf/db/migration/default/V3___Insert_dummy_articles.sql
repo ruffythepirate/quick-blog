4c3ed2cf10ca1216d2966f09a466309c5454d6b3
ruffy <johan.ellfries@gmail.com>
2018-09-11 07:11:31 +0200

    Add some dummy data for test development

---
+++ conf/db/migration/default/V3___Insert_dummy_articles.sql
@@ -0,0 +1,4 @@
+insert into articles (title, text) values ('Investigating further into new AngularJS version', '...');
+insert into articles (title, text) values ('New features in AWS Sept 19th', '...');
+insert into articles (title, text) values ('Using CDS the good and bad', '...');
+insert into articles (title, text) values ('Docker and integration tests', '...');

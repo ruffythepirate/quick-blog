8e4067ca698b5f392f779b50db530b87bf9e54ab
ruffy <johan.ellfries@gmail.com>
2018-10-13 08:06:07 +0200

    Fix jest linting.

--- frontend/src/components/editor/editor.spec.js
+++ frontend/src/components/editor/editor.spec.js
@@ -1,14 +1,8 @@
 import { shallowMount } from '@vue/test-utils';
 import cut from './editor.vue';
 
-jest.mock('lodash/debounce', () => {
-  console.log('banana pancake');
-  return jest.fn();
-});
-debounceMock = require('lodash/debounce');
-
-
-let debounceMock;
+jest.mock('lodash/debounce', () => jest.fn());
+const debounceMock = require('lodash/debounce');
 
 const TEXT_AREA_SELECTOR = '#editor textarea';
 
@@ -32,15 +26,12 @@ describe('editor', () => {
   });
 
   it('calls debounce on text change', () => {
-    console.log('start of test');
     const wrapper = factory();
 
     const textarea = wrapper.find(TEXT_AREA_SELECTOR);
     textarea.setValue('new value');
 
-    console.log('before expect');
     expect(debounceMock).toHaveBeenCalled();
-    console.log('after expect');
   });
 
   it('renders markdown when debounce method is called', () => {

eaa9fb5e2b6bba1fb018ec8026ab3e8088113535
ruffy <johan.ellfries@gmail.com>
2018-10-21 09:34:14 +0200

    Partly enable hot reload.

--- frontend/build/webpack.config.dev.js
+++ frontend/build/webpack.config.dev.js
@@ -1,9 +1,20 @@
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack.config.base');
 
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = merge(baseConfig, {
   mode: 'development',
   devServer: {
     port: 3000
-  }
+  },
+  plugins : [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin( {
+      filename: 'index.html',
+      template: 'index.html',
+      inject: true
+    })
+  ]
 });
